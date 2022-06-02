d9869a163e1c9d4f2ad5247c2bf8eee13b315efe
pooja-yadav06 <poojasyadav2@gmail.com>
2022-06-02 11:45:59 +0530

    assignment-nodejs1

--- src/util/helper.js
+++ src/util/helper.js
@@ -1,8 +1,11 @@
-const log = function printDate(){
-    console.log("todays date is"+ " "+ 1)
+const log = function(){
+    let currentDate=new Date()
+    console.log(currentDate)
 }
-   const log1= function printMonth(){
-        console.log("june")
+   const log1= function(){
+       let currentdate=new Date()
+       let currentMonth=currentdate.getMonth() + 1//since month start from 0 index hence added 1
+        console.log("The current month is" + currentMonth)
     }
        const log2=function getBatchInfo(){
             console.log("radon" ,"","W3D1" ,"","the topic for today is Nodejs module system" )
