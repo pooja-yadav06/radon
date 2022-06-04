a898d6215a3c8fe5e898b597420bf51b3ea2a12a
pooja-yadav06 <poojasyadav2@gmail.com>
2022-06-04 12:51:52 +0530

    assignment-api

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const { route } = require('express/lib/application');
+const res = require('express/lib/response');
 const router = express.Router();
 
 router.get('/students/:name', function(req, res) {
@@ -7,9 +9,60 @@ router.get('/students/:name', function(req, res) {
     res.send(studentName)
 })
 
-router.get("/random" , function(req, res) {
-    res.send("hi there")
+
+
+
+
+
+router.get('/movies', function(req, res) {
+    let movieList=["Dangal","dhakad","sultan","mohabbatein"]
+    res.send(movieList)
 })
+
+
+router.get('/movie/:index', function(req,res) {
+    let movie=["mard","lawaris","sharabi","yarana"]
+    if(req.params.index>3){
+        res.send("use valid index")
+        return;
+} else{
+    res.send(movie[req.params.index])}
+    return;
+})
+
+
+router.get('/films', function(req,res){
+    let film=[{
+        "id":1,"name":"bahubali"},{
+        "id":2,"name":"kgf"},{
+        "id":3,"name":"bahubali 2"},{
+         "id":4,"name":"kgf 2"},
+        ]
+        res.send({film})
+})
+
+
+router.get('/film/:Id',function(req,res){
+    let filmId=[{
+        "id":1,"name":"the lord of ring"},{
+            "id":2,"name":"iron man"},{
+                "id":3,"name":"wonder women"},{
+                    "id":4,"name":"batman"}
+]
+if(req.params.Id<5){
+res.send(filmId[req.params.Id-1].name)
+return;
+}
+else{
+res.send("id not exits")
+return; 
+}
+})
+
+
+
+
+
 
 
 router.get("/test-api" , function(req, res) {
