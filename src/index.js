af4d7e69d6e3cc8f154ef784d86b674e066107fe
pooja-yadav06 <poojasyadav2@gmail.com>
2022-06-11 21:51:49 +0530

    assignment/middleware-1

--- src/index.js
+++ src/index.js
@@ -7,21 +7,40 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const assignmentMW= function (req,res,next){
+    var currentDate = new Date();
+    var datetime = currentDate.getDate() + " "
+                   + (currentDate.getMonth()+1) + " "
+                   + currentDate.getFullYear() + " "
+                   + currentDate.getHours() + ":"
+                   + currentDate.getMinutes() + ":"
+                   + currentDate.getSeconds();
+    let ip= req.ip
+    let url= req.originalUrl
+    
+    console.log(`${datetime} ${ip}  ${url}`)
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
+    next()
+}
+
+app.use(assignmentMW)
+
+
+mongoose.connect("mongodb+srv://pooja-yadav:poojamongodb@cluster0.xepnr.mongodb.net/test", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
 .catch ( err => console.log(err) )
 
-app.use (
-    function (req, res, next) {
-        console.log ("inside GLOBAL MW");
-        next();
-  }
-  );
+// app.use (
+//     function (req, res, next) {
+//         console.log ("inside GLOBAL MW");
+//         next();
+//   }
+//   );
 
-app.use('/', route);
+// app.use('/', route);
+
 
 
 app.listen(process.env.PORT || 3000, function () {
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,18 +1,23 @@
 const express = require('express');
 const router = express.Router();
 // const UserModel= require("../models/userModel.js")
-const UserController= require("../controllers/userController")
-const BookController= require("../controllers/bookController")
-const commonMW = require ("../middlewares/commonMiddlewares")
+// const UserController= require("../controllers/userController")
+// const BookController= require("../controllers/bookController")
+// const commonMW = require ("../middlewares/commonMiddlewares")
 
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
+});
+
+router.get('/createUser',function(req,res){
+    res.send("the current path is")
+    
 })
 
 
 
 
-router.post("/createBook", BookController.createBook  )
+// router.post("/createBook", BookController.createBook  )
 
 
 
@@ -48,7 +53,7 @@ router.post("/createBook", BookController.createBook  )
 
 
 
-router.get("/basicRoute", commonMW.mid1, commonMW.mid2, commonMW.mid3, commonMW.mid4, UserController.basicCode)
+// router.get("/basicRoute", commonMW.mid1, commonMW.mid2, commonMW.mid3, commonMW.mid4, UserController.basicCode)
 
 
 
